92d85d1097efa8e45f32cc7b3c106f637b100df6
Abdulmalik <kxgmar@gmail.com>
2026-09-25 16:14:18 +0300

    chore(web): remove the pages used to test error reporting
    Both kinds of failure now reach the panel from a deployed function.

--- apps/web/src/app/debug-detached/page.tsx
+++
@@ -1,7 +0,0 @@
-export const dynamic = "force-dynamic";
-
-// A rejection nobody awaits, which Next never sees as a render error.
-export default function Page() {
-  void Promise.reject(new Error("detached rejection during render"));
-  return <p>rendered</p>;
-}
